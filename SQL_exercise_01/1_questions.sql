7d7a4b136f90ce75996210f53327a4e2e6183a87
nikhilgehlot95 <80015524+nikhilgehlot95@users.noreply.github.com>
2022-08-04 12:54:29 +0530

    Update 1_questions.sql

--- SQL_exercise_01/1_questions.sql
+++ SQL_exercise_01/1_questions.sql
@@ -3,7 +3,7 @@
 -- 1.2 Select the names and the prices of all the products in the store.
 -- 1.3 Select the name of the products with a price less than or equal to $200.
 -- 1.4 Select all the products with a price between $60 and $120.
--- 1.5 Select the name and price in cents represented as prices (i.e., the price must be multiplied by 100) with a price between $60 and $120and sort first by price (in descending order), and then by name (in ascending order)................
+Select the name and price in cents represented as prices (i.e., the price must be multiplied by 100) with a price between $60 and $180 and sort first by price (in descending order), and then by name (in ascending order)................
 -- 1.6 Compute the average price of all the products.
 -- 1.7 Compute the average price of all products with manufacturer code equal to 2.
 -- 1.8 Compute the number of products with a price larger than or equal to $180.
