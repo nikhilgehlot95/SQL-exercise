3cda6587c7cba773bca53783450782524cccc42f
nikhilgehlot95 <80015524+nikhilgehlot95@users.noreply.github.com>
2022-08-04 16:19:42 +0530

    Update 1_questions.sql

--- SQL_exercise_01/1_questions.sql
+++ SQL_exercise_01/1_questions.sql
@@ -15,11 +15,12 @@
 -- 1.17 Add a new product: Loudspeakers, $70, manufacturer 2.
 -- 1.18 Update the name of product 8 to "Laser Printer".
 -- 1.19 Apply a 10% discount to all products.
--- 1.20 Apply a 10% discount to all products with a price larger than or equal to $120.
 
 
 
 Select the name and price in cents represented as prices (i.e., the price must be multiplied by 100) with a price between $60 and $180 and sort first by price (in descending order), and then by name (in ascending order)................
 Select the average price of each manufacturer's products, showing the manufacturer's name,have an average price larger than or equal to $150.
 Select the name and price of the cheapest product.
+Apply a 10% discount to all products with a price larger than or equal to $120.
+
 
